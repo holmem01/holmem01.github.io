8d197f278a00562bac0a8f05357342aa3749e579
holmem01 <holmem01@luther.edu>
2015-10-23 13:21:22 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,1 +1,32 @@
+<script type = "text/javascript" src = "http://bnmnetp.github.io/CS130/listsavers.js"></script>
 
+window.onload=function() {
+        restorelist('alist', doneTask)
+      }
+makeNewTask = function() {
+        var pri, inputText, ulist, string;
+      
+        ulist = document.querySelector("#alist");
+        task = document.createElement("li");
+        checkb = document.createElement("input");
+        pri = document.querySelector("#priority").value;
+        task.className = pri;
+        inputText = document.querySelector("#newtask").value;
+        taskText = document.createTextNode(inputText);
+    doneTask = function() {
+         if (this.checked) {
+           this.parentNode.classList.add("done");
+         } else {
+           this.parentNode.classList.remove("done");
+         }
+         localSave('alist')
+       }    
+        checkb.type = "checkbox";
+        checkb.onclick = doneTask;
+        task.appendChild(checkb);
+        task.appendChild(taskText);
+        ulist.appendChild(task);
+        localSave('alist')
+      }
+
+  
